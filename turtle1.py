90aee9f7a8be34c77ffdc98d9ffb199488d9ad76
leenakhalili <engleena@yahoo.com>
2021-12-02 13:27:56 +0300

    Turtle graphics

--- turtle1.py
+++ turtle1.py
@@ -1,8 +1,6 @@
-import turtle
-wn = turtle.Screen()
-alex = turtle.Turtle()
-alex.forward(150)
-alex.left(90)
-alex.forward(75)
-alex.salary = 50000
-print(alex.salary)
+import turtle             # allows us to use the turtles library
+wn = turtle.Screen()      # creates a graphics window
+alex = turtle.Turtle()    # create a turtle named alex
+alex.forward(150)         # tell alex to move forward by 150 units
+alex.left(90)             # turn by 90 degrees
+alex.forward(75)          # complete the second side of a rectangle
